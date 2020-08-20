d03e9b452cb38f27326c9cd915f45d80d1dd821a
larry li <li.chuan.huan@gmail.com>
2020-08-20 21:52:52 +0800

    setStone, getBoard

--- game_tictacton/TicTacTon.sol
+++ game_tictacton/TicTacTon.sol
@@ -4,10 +4,11 @@ contract Event {
    
     uint8 public boardSize = 3;
 
-    address[3][3] borad;
+    address[3][3] board;
 
     address public player1;
     address public player2;
+    address activePlayer;
 
     constructor () public{
         player1 = msg.sender;
@@ -17,6 +18,29 @@ contract Event {
         //確認玩家2未被使用
         assert(player2 == address(0));
         player2 = msg.sender;
+        activePlayer = player2;
+    }
+
+    function getBoard() view public returns(address[3][3]){
+        return board;
+    }
+
+    function setStone(uint8 _x, uint8 _y) public {
+        require(_x < 3);
+        require(_y < 3);
+        require(board[_x][_y] == address(0));
+        require(activePlayer == msg.sender);
+        board[_x][_y] = msg.sender;
+
+       changePlayer();
+    }
+
+    function changePlayer() private {
+        if(activePlayer == player1){
+            activePlayer = player2;
+        }else{
+            activePlayer = player1;
+        }
     }
    
     
