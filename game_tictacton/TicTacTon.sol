b334e0cebce713bf1f219283ad76df1c0dda4064
larry li <li.chuan.huan@gmail.com>
2020-08-20 22:35:23 +0800

    create function for check result

--- game_tictacton/TicTacTon.sol
+++ game_tictacton/TicTacTon.sol
@@ -10,6 +10,8 @@ contract Event {
     address public player2;
     address activePlayer;
 
+    uint8 public putStoneTimes = 0;
+
     constructor () public{
         player1 = msg.sender;
     }
@@ -21,7 +23,7 @@ contract Event {
         activePlayer = player2;
     }
 
-    function getBoard() view public returns(address[3][3]){
+    function getBoard() view public returns(address[3][3] memory){
         return board;
     }
 
@@ -32,7 +34,12 @@ contract Event {
         require(activePlayer == msg.sender);
         board[_x][_y] = msg.sender;
 
-       changePlayer();
+        //下子數加1
+        putStoneTimes++;
+
+        checkResult(_x, _y);//TODO 需要依結果做處理
+       
+        changePlayer();
     }
 
     function changePlayer() private {
@@ -40,6 +47,63 @@ contract Event {
             activePlayer = player2;
         }else{
             activePlayer = player1;
+        }
+    }
+
+
+    //檢查結果
+    function checkResult(uint8 _x, uint8 _y) private view{
+        //檢查列
+        for(uint8 i = 0; i < boardSize; i++){
+            if(board[i][_y] != activePlayer){
+                break;
+            }
+
+            if(i == boardSize - 1){
+                // TODO 贏了
+            }
+        }
+
+        //檢查行
+        for(uint8 i = 0; i < boardSize; i++){
+            if(board[_x][i] != activePlayer){
+                break;
+            }
+
+            if(i == boardSize - 1){
+                // TODO 贏了
+            }
+        }
+
+        //檢查對角線
+        if(_x == _y){
+            for(uint8 i = 0; i< boardSize; i++ ){
+                if(board[i][i] != activePlayer){
+                    break;
+                }
+
+                if(i == boardSize - 1){
+                // TODO 贏了
+                }
+            }
+        }
+
+        //斜對角線
+        if((_x+_y) == boardSize - 1){
+            for(uint8 i = 0; i< boardSize; i++ ){
+                if(board[i][(boardSize -1) - i] != activePlayer){
+                    break;
+                }
+
+                if(i == boardSize - 1){
+                // TODO 贏了
+                }
+            }
+        }
+
+        //和局
+        if(putStoneTimes == boardSize * boardSize){
+            // TODO 和局
         }
     }
    
