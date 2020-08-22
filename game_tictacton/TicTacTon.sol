f75e7dacfba10c2c4107839436480daa6daf8eb6
larry li <li.chuan.huan@gmail.com>
2020-08-22 15:16:24 +0800

    handle game end

--- game_tictacton/TicTacTon.sol
+++ game_tictacton/TicTacTon.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.4.22 <0.7.0;
 contract Event {
    
     uint8 public boardSize = 3;
+    bool gameActive;
 
     address[3][3] board;
 
@@ -10,7 +11,7 @@ contract Event {
     address public player2;
     address activePlayer;
 
-    uint8 public putStoneTimes = 0;
+    uint8 public putStoneCounter = 0;
 
     constructor () public{
         player1 = msg.sender;
@@ -19,6 +20,7 @@ contract Event {
     function joinGame() public{
         //確認玩家2未被使用
         assert(player2 == address(0));
+        gameActive = true;
         player2 = msg.sender;
         activePlayer = player2;
     }
@@ -28,18 +30,31 @@ contract Event {
     }
 
     function setStone(uint8 _x, uint8 _y) public {
-        require(_x < 3);
-        require(_y < 3);
         require(board[_x][_y] == address(0));
         require(activePlayer == msg.sender);
+        assert(gameActive);
+        assert(_x < 3);
+        assert(_y < 3);
         board[_x][_y] = msg.sender;
 
         //下子數加1
         putStoneTimes++;
 
-        checkResult(_x, _y);//TODO 需要依結果做處理
-       
+        if(checkResult(_x, _y)){
+            return;
+        }
         changePlayer();
+    }
+
+    function setWinner(address winner) private {
+        gameActive = false;
+        // TODO 玩家勝利事件
+        // TODO 送錢給贏家
+    }
+
+    function setDraw() private{
+         gameActive = false;
+        // TODO 玩家和局事件
     }
 
     function changePlayer() private {
@@ -52,7 +67,8 @@ contract Event {
 
 
     //檢查結果
-    function checkResult(uint8 _x, uint8 _y) private view{
+    //有結果回傳 True，沒結果回傳 false
+    function checkResult(uint8 _x, uint8 _y) private view returns (bool){
         //檢查列
         for(uint8 i = 0; i < boardSize; i++){
             if(board[i][_y] != activePlayer){
@@ -60,7 +76,8 @@ contract Event {
             }
 
             if(i == boardSize - 1){
-                // TODO 贏了
+                setWinner(activePlayer);
+                return true;
             }
         }
 
@@ -71,7 +88,8 @@ contract Event {
             }
 
             if(i == boardSize - 1){
-                // TODO 贏了
+                setWinner(activePlayer);
+                return true;
             }
         }
 
@@ -83,7 +101,8 @@ contract Event {
                 }
 
                 if(i == boardSize - 1){
-                // TODO 贏了
+                    setWinner(activePlayer);
+                    return true;
                 }
             }
         }
@@ -96,15 +115,19 @@ contract Event {
                 }
 
                 if(i == boardSize - 1){
-                // TODO 贏了
+                    setWinner(activePlayer);
+                    return true;
                 }
             }
         }
 
         //和局
-        if(putStoneTimes == boardSize * boardSize){
-            // TODO 和局
+        if(putStoneCounter == (boardSize ** 2)){
+           setDraw();
+           return true;
         }
+
+        return false;
     }
    
     
